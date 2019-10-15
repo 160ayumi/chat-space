8f44437bd687ecedceaefbfce03e664e33b2f865
160ayumi <160ayumi@gmail.com>
2019-10-15 12:37:30 +0900

    自動更新機能２

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -3,7 +3,7 @@ $(function(){
 
     var image = data.image ? `<img src="${data.image}">` : image = "";
 
-    var html = `<div class="message">
+    var html = `<div class="message" data-message-id="${data.id}">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
                       ${data.user}
@@ -46,5 +46,39 @@ $(function(){
     .fail(function(){
       alert('error');
     })
+  
   });
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    if (window.location.href.match(/messages/)){
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+     //追加するHTMLの入れ物を作る
+     //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+     messages.forEach(function (message){
+      
+     //メッセージが入ったHTMLを取得
+     var insertHTML = buildData(message);
+     //メッセージを追加
+     $('.messages').append(insertHTML);
+    })
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+      })
+    .fail(function() {
+      console.log('error');
+    });
+  }
+  };
+  setInterval(reloadMessages, 5000);
+  
+  
 });
