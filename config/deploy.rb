ae59603069c713485924c922228d59093a2854e4
160ayumi <160ayumi@gmail.com>
2019-10-17 14:34:36 +0900

    curriculum7

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,13 +40,13 @@
 
 # config valid only for current version of Capistrano
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.1.0'
+lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space''
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:<160ayumi>/<chat-space>.git'
+set :repo_url,  'git@github.com:160ayumi/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -56,7 +56,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/.ssh/curriculum.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['/.ssh/curriculum.pem']  
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
